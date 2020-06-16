42be5e66cd04f201d2afe95f74abb0fee0dccdf3
J Fav <jfavorito@outlook.com>
2020-06-15 20:21:34 -0700

    made aesthetic changes

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     let userData;
     const userFname = document.querySelector("#user-fname");
     const userLname = document.querySelector("#user-lname");
+    const userEmail = document.querySelector("#user-email");
     const userWage = document.querySelector("#user-wage");
     const userTime = document.querySelector("#user-time");
     const userEarnings = document.querySelector("#user-earnings");
@@ -30,6 +31,7 @@ $(document).ready(function () {
     const displayUserData=async()=>{
         userFname.textContent = userData.first_name;
         userLname.textContent = userData.last_name;
+        userEmail.textContent = userData.email;
         userWage.textContent = userData.wage;
         userTime.textContent = userData.total_time;
         userEarnings.textContent = userData.total_earnings;
