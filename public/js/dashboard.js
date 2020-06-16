3db22da1fa21f05e93ae66b24f2d32b51220c633
J Fav <jfavorito@outlook.com>
2020-06-15 19:27:06 -0700

    uploading working project

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,15 +2,46 @@ $(document).ready(function () {
     // var userIdInput = $("#userId");
     var userList = $("tbody");
     var userContainer = $(".user-container");
+    let userData;
+    // const userId = document.querySelector("#user-id")
+    const userFname = document.querySelector("#user-fname");
+    const userLname = document.querySelector("#user-lname");
+    const userWage = document.querySelector("#user-wage");
+    const userTime = document.querySelector("#user-time");
+    const userEarnings = document.querySelector("#user-earnings");
 
 
 
-    // $(document).on("click", ".delete-user", handleDeleteButtonPress);
+    const updateUserData = async () => {
+        await
+        $.get("/api/tracker/user_data").then(function (data) {
+            console.log(data);
+            userData = data;
+        });
+    }
 
-    // Getting the initial list of users
-    // getUsers();
 
-    // Set the defaults for the dashboard-table display
+
+   const main = async () =>{
+       try {
+        await updateUserData();
+        await displayUserData();
+       } catch (error) {
+           console.log(error);
+       }
+   }
+
+    const displayUserData=async()=>{
+        // userId.textContent = userData.id;
+        userFname.textContent = userData.first_name;
+        userLname.textContent = userData.last_name;
+        userWage.textContent = userData.wage;
+        userTime.textContent = userData.total_time;
+        userEarnings.textContent = userData.total_earnings;
+    }
+
+
+
     $("#dashboard-table").DataTable({
         "paging": false,
         "searching": false,
@@ -25,111 +56,6 @@ $(document).ready(function () {
         }
     });
 
-    // Function for retrieving users and getting them ready to be rendered to the page
-    function getUsers() {
-        $.get("/api/dashboard", function (data) {
-            var rowsToAdd = [];
-            for (var i = 0; i < data.length; i++) {
-                rowsToAdd.push(createUserRow(data[i]));
-            }
-            renderUserList(rowsToAdd);
-        });
-    }
-    
+    main();
 
-    // $("#submitUser").on("click", function (event) {
-    //     console.log(userID);
-
-    //     event.preventDefault();
-
-    //     getUserById(userID);
-    // });
-    // var userID = $("#enteredUserId").val().trim();
-
-    let userID;
- 
-    $.get("/api/dashboard/:id").then(function(data) {
-        console.log("I am in the .get(//api/dashboard/:id/)");
-        console.log(data);
-        userID = data.id;
-        console.log(userID);
-
-        event.preventDefault();
-
-        getUserById(userID);
-      });
-
-    function getUserById(userID) {
-
-        console.log(userID);
-
-        $.ajax({
-            method: "GET",
-            url: "/api/dashboard/" + userID
-            
-        })
-            .then(getUsers);
-    }
-
-    
-    // Function for creating a new list row for users
-    function createUserRow(userData) {
-        var newTr = $("<tr>");
-        newTr.data("user", userData);
-        newTr.append("<td>" + userData.id + "</td>");
-        newTr.append("<td>" + userData.first_name + "</td>");
-        newTr.append("<td>" + userData.last_name + "</td>");
-        newTr.append("<td>" + userData.time_worked + "</td>");
-        newTr.append("<td> $" + userData.wage + "</td>");
-        newTr.append("<td>" + userData.total_time + "</td>");
-        newTr.append("<td> $" + userData.total_earnings + "</td>");
-        // newTr.append("<td><a style='cursor:pointer;color:red' class='delete-user'>Delete User</a></td>");
-        // newTr.append("<td><button type='button' class='btn-red delete-user btn-sm m-0'>Delete User</button></td>");
-        return newTr;
-    }
-
-
-
-    // A function for rendering the list of authors to the page
-    function renderUserList(rows) {
-        // $("#dashboard-table").DataTable({
-        //     "retrieve": true,
-        //     "scrollX": true,
-        //     "searching": false,
-        //     "language": {
-        //         "lengthMenu": "",
-        //         "zeroRecords": "",
-        //         "info": "",
-        //         "infoEmpty": ""
-        //     }
-        // });
-        userList.children().not(":last").remove();
-        userContainer.children(".alert").remove();
-        if (rows.length) {
-            console.log(rows);
-            userList.prepend(rows);
-        }
-        else {
-            renderEmpty();
-        }
-    }
-
-    // Function for handling what to render when there are no users
-    function renderEmpty() {
-        var alertDiv = $("<div>");
-        alertDiv.addClass("alert alert-danger");
-        alertDiv.text("You must create an User before you can create a Table of Users.");
-        userContainer.append(alertDiv);
-    }
-
-    // Function for handling what happens when the delete button is pressed
-    // function handleDeleteButtonPress() {
-    //     var listItemData = $(this).parent("td").parent("tr").data("account");
-    //     var id = listItemData.id;
-    //     $.ajax({
-    //         method: "DELETE",
-    //         url: "/api/dashboard/" + id
-    //     })
-    //         .then(getUsers);
-    // }
 });
--- public/js/tracker.js
+++ public/js/tracker.js
@@ -1,145 +1,146 @@
-$(document).ready(function() {
-const displayTime = document.querySelector("#displayTime");
-displayTime.textContent = moment().format("dddd, MMMM DD, YYYY hh:mm A");
-const clockInBtn = document.querySelector("#clock-in");
-const clockOutBtn = document.querySelector("#clock-out");
-const breakBtn = document.querySelector("#break-btn");
-const clockInTimeEl = document.querySelector("#clock-in-time");
-const clockOutTimeEl = document.querySelector("#clock-out-time");
-const currentTimeEl = document.querySelector("#current-time");
-currentTimeEl.textContent = "Current Time: " + moment().format("h:mm:ss A");
-const timeClockedInEl = document.querySelector("#time-clocked-in");
-const todaysEarnings = document.querySelector("#money-earned");
-// const users = require("./users");
+$(document).ready(function () {
+    const displayTime = document.querySelector("#displayTime");
+    displayTime.textContent = moment().format("dddd, MMMM DD, YYYY hh:mm A");
+    const clockInBtn = document.querySelector("#clock-in");
+    const clockOutBtn = document.querySelector("#clock-out");
+    // const breakBtn = document.querySelector("#break-btn");
+    const clockInTimeEl = document.querySelector("#clock-in-time");
+    const clockOutTimeEl = document.querySelector("#clock-out-time");
+    const currentTimeEl = document.querySelector("#current-time");
+    currentTimeEl.textContent = "Current Time: " + moment().format("h:mm:ss A");
+    // const timeClockedInEl = document.querySelector("#time-clocked-in");
+    const todaysEarnings = document.querySelector("#money-earned");
+    // const users = require("./users");
 
-let breakTime = false;
-let resumeClockInTime;
-let totalSeconds = 0;
-let start;
-let clockInTime;
-let clockOutTime;
-let clickedClockOut = false;
-let timerCount = 0;
-let wage;
-let now = moment();
-displayTimeTimer();
-currentTimeTimer();
+    let breakTime = false;
+    let resumeClockInTime;
+    let totalSeconds = 0;
+    let start;
+    let clockInTime;
+    let clockOutTime;
+    let clickedClockOut = false;
+    let timerCount = 0;
+    let wage;
+    let now = moment();
+    displayTimeTimer();
+    currentTimeTimer();
 
-// var minutesPassed = moment().diff(start, 'minutes');
+    // var minutesPassed = moment().diff(start, 'minutes');
 
-function displayLapsedTime() {
+    function displayLapsedTime() {
 
-    const hoursLabel = document.querySelector("#hours");
-    var minutesLabel = document.getElementById("minutes");
-    var secondsLabel = document.getElementById("seconds");
-    if (totalSeconds !== 0) {
-        totalSeconds = resumeClockInTime;
-    }
-    timer = setInterval(setTime, 1000);
-
-    function setTime() {
-        if (breakTime === true || clickedClockOut === true) {
-            resumeClockinTime = totalSeconds;
-            console.log(resumeClockInTime);
-            clearInterval(timer);
+        const hoursLabel = document.querySelector("#hours");
+        var minutesLabel = document.getElementById("minutes");
+        var secondsLabel = document.getElementById("seconds");
+        if (totalSeconds !== 0) {
+            totalSeconds = resumeClockInTime;
         }
-        ++totalSeconds;
-        secondsLabel.innerHTML = pad(totalSeconds % 60);
-        minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
-        hoursLabel.innterHTML = pad(parseInt(totalSeconds / 3600));
-        displayEarnings();
-    }
+        timer = setInterval(setTime, 1000);
 
-    function pad(val) {
-        var valString = val + "";
-        if (valString.length < 2) {
-            return "0" + valString;
-        } else {
-            return valString;
+        function setTime() {
+            if (breakTime === true || clickedClockOut === true) {
+                resumeClockinTime = totalSeconds;
+                console.log(resumeClockInTime);
+                clearInterval(timer);
+            }
+            ++totalSeconds;
+            secondsLabel.innerHTML = pad(totalSeconds % 60);
+            minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+            hoursLabel.innterHTML = pad(parseInt(totalSeconds / 3600));
+            displayEarnings();
         }
-    }
 
-}
-
-let userData;
-let totalEarnings;
-
-const displayEarnings=()=> {
-    totalEarnings = ((userData.wage/ 3600) * totalSeconds).toFixed(2);
-    todaysEarnings.textContent = totalEarnings;
-}
-
-let newTime;
-let newEarnings;
-
-const addTime=(time)=>{
-    let oldTime=parseInt(userData.total_time);
-    newTime=oldTime+time;
-}
-
-const addEarnings=(earnings)=>{
-    let oldEarnings=parseFloat(userData.total_earnings);
-    console.log(oldEarnings)
-    newEarnings=oldEarnings+parseFloat(earnings);
-}
-
-const updateUserData=()=>{
-$.get("/api/tracker/user_data").then(function(data) {
-    console.log(data);
-    userData=data;
-  });
-}
-
-function displayTimeTimer() {
-    timer = setInterval(() => {
-        displayTime.textContent = moment().format("dddd, MMMM DD, YYYY hh:mm A");
-    }, 1000);
-}
-function currentTimeTimer() {
-    timer = setInterval(() => {
-        currentTimeEl.textContent = "Current Time: " + moment().format("h:mm:ss A");
-    }, 1000);
-}
-
-clockInBtn.addEventListener("click", function (e) {
-    updateUserData();
-    start = moment();
-    clockInTime = moment().format("hh:mm:ss");
-    clockInTimeEl.textContent = "    " + clockInTime;
-    localStorage.setItem("clockIn", moment().format("hh:mm:ss"));
-    clockInBtn.disabled = true;
-    clockOutBtn.disabled = false;
-    breakBtn.disabled = false;
-    displayLapsedTime();
-});
-clockOutBtn.addEventListener("click", function (e) {
-    updateUserData();
-    clockOutTime = moment().format("hh:mm:ss");
-    clockOutTimeEl.textContent = "    " + clockOutTime;
-    localStorage.setItem("clockOut", moment().format("hh:mm:ss"));
-    clickedClockOut = true;
-    clockInBtn.disabled = false;
-    clockOutBtn.disabled = true;
-    addTime(totalSeconds);
-    addEarnings(totalEarnings);
-    let newUserData={
-        total_earnings: newEarnings,
-        total_time: newTime
-    }
-    $.ajax({
-        url: '/api/tracker/user_data',
-        type: 'PUT',
-        data: newUserData,
-        success: function(data) {
-          alert('Load was performed.');
+        function pad(val) {
+            var valString = val + "";
+            if (valString.length < 2) {
+                return "0" + valString;
+            } else {
+                return valString;
+            }
         }
-      });
 
-});
+    }
 
-breakBtn.addEventListener("click", (e) => {
+    let userData;
+    let totalEarnings;
 
-});
+    const displayEarnings = () => {
+        totalEarnings = ((userData.wage / 3600) * totalSeconds).toFixed(2);
+        todaysEarnings.textContent = totalEarnings;
+    }
+
+    let newTime;
+    let newEarnings;
+
+    const addTime = (time) => {
+        let oldTime = parseInt(userData.total_time);
+        newTime = oldTime + time;
+    }
+
+    const addEarnings = (earnings) => {
+        let oldEarnings = parseFloat(userData.total_earnings);
+        console.log(oldEarnings)
+        newEarnings = oldEarnings + parseFloat(earnings);
+    }
+
+    const updateUserData = () => {
+        $.get("/api/tracker/user_data").then(function (data) {
+            console.log(data);
+            userData = data;
+        });
+    }
+
+    function displayTimeTimer() {
+        timer = setInterval(() => {
+            displayTime.textContent = moment().format("dddd, MMMM DD, YYYY hh:mm A");
+        }, 1000);
+    }
+    function currentTimeTimer() {
+        timer = setInterval(() => {
+            currentTimeEl.textContent = "Current Time: " + moment().format("h:mm:ss A");
+        }, 1000);
+    }
+
+    clockInBtn.addEventListener("click", function (e) {
+        updateUserData();
+        start = moment();
+        clockInTime = moment().format("hh:mm:ss");
+        clockInTimeEl.textContent = "    " + clockInTime;
+        localStorage.setItem("clockIn", moment().format("hh:mm:ss"));
+        clockInBtn.disabled = true;
+        clockOutBtn.disabled = false;
+        // breakBtn.disabled = false;
+        displayLapsedTime();
+    });
+    clockOutBtn.addEventListener("click", function (e) {
+        updateUserData();
+        clockOutTime = moment().format("hh:mm:ss");
+        clockOutTimeEl.textContent = "    " + clockOutTime;
+        localStorage.setItem("clockOut", moment().format("hh:mm:ss"));
+        clickedClockOut = true;
+        clockInBtn.disabled = false;
+        clockOutBtn.disabled = true;
+        addTime(totalSeconds);
+        addEarnings(totalEarnings);
+        let newUserData = {
+            total_earnings: newEarnings,
+            total_time: newTime
+        }
+        $.ajax({
+            url: '/api/tracker/user_data',
+            type: 'PUT',
+            data: newUserData,
+            success: function (data) {
+                alert('Load was performed.');
+            }
+        });
+        location.reload();
+        return false;
+    });
+
+    // breakBtn.addEventListener("click", (e) => {
+
+    // });
 
 });
 
